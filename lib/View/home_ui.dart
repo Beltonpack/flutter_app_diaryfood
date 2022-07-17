a30c4fedd683ecdd730af137de0bb9d0b8455aca
Beltonpack <s6452g10001@sau.ac.th>
2022-07-17 10:07:06 +0700

    SCV 4.0: Create models, diaryfood by Pack

--- lib/View/home_ui.dart
+++ lib/View/home_ui.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 
-class HomeUI extends StatefulWidget {
+class HomeUI extends StatelessWidget {
   const HomeUI({Key? key}) : super(key: key);
 
   @override
-  State<HomeUI> createState() => _HomeUIState();
-}
-
-class _HomeUIState extends State<HomeUI> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 44, 39, 176),
-    );
+    return Scaffold();
   }
 }
---
+++ lib/View/message_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessageUI_State extends StatefulWidget {
+  const MessageUI_State({Key? key}) : super(key: key);
+
+  @override
+  State<MessageUI_State> createState() => _MessageUI_StateState();
+}
+
+class _MessageUI_StateState extends State<MessageUI_State> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
